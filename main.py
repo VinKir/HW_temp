dedef9d0887373561a1405b5f2d4a9fe3992652d
VinKir <VinitKirill@gmail.com>
2023-10-31 19:44:58 +0500

    other func for plot

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-x = np.linspace(-2, 2, 50)
-y = [math.sin(i) * math.cos(i) for i in x]
+x = np.linspace(-10, 4, 100)
+y = [math.log(i) if i>0 else 0 + math.cos(i) for i in x]
 
 plt.xlabel("x")
 plt.ylabel("y")
